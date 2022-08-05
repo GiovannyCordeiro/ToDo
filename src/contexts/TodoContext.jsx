e13af09faa975aa8b4cba9c4e5b8395b01fafa5b
Giovanny <giovannycordeiropb@gmail.com>
2022-08-05 11:22:22 -0300

    setup-project

---
+++ src/contexts/TodoContext.jsx
@@ -0,0 +1,16 @@
+import{createContext, useState} from 'react';
+
+const TodosContext = createContext();
+
+const TodoContextProvider = ({children}) => {
+
+  const [listTodos, setListsTodos] = useState([])
+
+  return(
+    <TodosContext.Provider>
+      {children}
+    </TodosContext.Provider>
+  )
+}
+
+export default TodoContextProvider;
